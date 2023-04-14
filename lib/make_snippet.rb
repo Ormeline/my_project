1655b6487a9962d9bde7a3cdaf33488265c5c236
Ormeline <ormelineluyengi@outlook.com>
2023-04-14 15:06:26 +0100

    Add exisiting file

---
+++ lib/make_snippet.rb
@@ -0,0 +1,6 @@
+def make_snippet(string)
+    arr = string.split(" ")
+end 
+
+puts make_snippet("I am leaning Ruby.")
+
---
+++ spec/make_snippet_spec.rb
@@ -0,0 +1,6 @@
+require 'make_snippet'
+
+RSpec.describe 'make_snippet' do
+    it "takes input and returns first five words followed by ellipsis" do
+        expect()
+end
