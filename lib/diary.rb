a862bf2d6cb2f0f7fcc46bc8f52f1b49a856d911
Julian Festing <julianfesting@MASTUDENT-Julian-Festing-C02G573RQ6L4.local>
2023-04-17 15:11:27 +0100

    did reading_time method

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,27 +1,29 @@
 class DiaryEntry
-    def initialize(title, contents) # title, contents are strings
+    def initialize(title, contents)
     @title = title
     @contents = contents
     end
   
     def title
-      # Returns the title as a string
       return @title
     end
   
     def contents
-      # Returns the contents as a string
       return @contents
     end
   
     def count_words
-      # Returns the number of words in the contents as an integer
+      word_array = @contents.split(" ")
+      word_array.count
     end
   
-    def reading_time(wpm) # wpm is an integer representing the number of words the
-                          # user can read per minute
-      # Returns an integer representing an estimate of the reading time in minutes
-      # for the contents at the given wpm.
+    def reading_time(wpm) 
+      word_array = @contents.split(" ")
+      
+      wps = wpm / 60
+      wps = wps * word_array.length
+      wpm = wps / 60.to_f
+      wpm.round(2)
     end
   
     def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -2,8 +2,22 @@ require "diary"
 
 RSpec.describe "diary" do 
     it "should initialize a tile and contents as a string" do
-    set_diary = DiaryEntry.new("meeting", "activities")
-    expect(set_diary.title).to eq "meeting"
-    expect(set_diary.contents).to eq "activities"
+      set_diary = DiaryEntry.new("meeting", "activities")
+      expect(set_diary.title).to eq "meeting"
+      expect(set_diary.contents).to eq "activities"
+    end
+
+    it 'Returns the number of words in the contents as an integer' do
+      set_diary = DiaryEntry.new("meeting", "activities")
+      set_diary_2 = DiaryEntry.new("meeting", "activities for the weekend")
+
+      expect(set_diary.count_words).to eq 1
+      expect(set_diary_2.count_words).to eq 4
+    end
+
+    it 'Returns an integer representing an estimate of the reading time in minute' do
+      set_diary = DiaryEntry.new("meeting", "activities for the weekend")
+      
+      expect(set_diary.reading_time(60)).to eq 0.07
     end
 end 
