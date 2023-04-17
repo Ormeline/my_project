202237f8fe22f61dff7ce41764c29f8b5816d75e
Ormeline <ormelineluyengi@outlook.com>
2023-04-17 12:20:17 +0100

    updated appointment_reminder.rb & spec file

--- lib/appointment_reminder.rb
+++ lib/appointment_reminder.rb
@@ -1,10 +1,9 @@
 def appointment_reminder(name, appointment_time)
-    if appointment_time 
-      return "#{name} you have an appointment at #{appointment_time}. You must attend!"
+    if appointment_time
+        return "#{name}you have an appointment at #{appointment_time}. You must attend!"
     else 
-      return "#{name}, you don't have an appointment scheduled."
+        return "#{name} no appointment has been scheduled."
     end 
-  end 
-  
-    puts appointment_reminder("Lola", "2:30pm on Monday")
-    puts appointment_reminder("Lola", nil)
+end
+
+puts appointment_reminder("Lola", "10:30 am on Monday. You must attend!")
--- spec/appointment_reminder_spec.rb
+++ spec/appointment_reminder_spec.rb
@@ -1,20 +1,12 @@
 require "appointment_reminder"
 
-RSpec.describe "appointment_reminder" do
-    it "returns a name and a message that the person has an appoitnment at a set time and they must attend" do
-        appointment_reminder_message = appointment_reminder("Lola", "10:30 am on Monday. You must attend!") do
-        expect(appointment_reminder_message).to eq "Lola, you have an appointment at 10:30 am on Monday. You must attend!"
-        end 
-    end
-
-    context "when no appointment time has been provided" do
-        it "returns a message stating no appointment has been scheduled" do
-            appointment_reminder_message = appointment_reminder("Lola", nil) 
-            expect(appointment_reminder_message).to eq "Lola, you don't have an appointment scheduled."
+RSpec.describe "appointment_reminder" do 
+    it "returns a name and a message that an appointment has been scheduled at a set time and day" do
+        appointment_reminder_message = appointment_reminder("Lola", "10:30 on Monday") do
+        expect(appointment_reminder_message).to eq "Lola, you have an appointment at 10:am on Monday. You must attend!"
         end
     end
 end
-
 
 
 
